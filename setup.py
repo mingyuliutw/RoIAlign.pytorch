0c14f9ab1d91c86657d09ef35f3ad6dccb7d1c1f
Ming-Yu Liu <mingyu.liu.tw@gmail.com>
2019-12-04 20:09:19 -0800

    Compile CUDA by default

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ modules = [
         )
 ]
 
-if torch.cuda.is_available():
-    modules.append(
-        CUDAExtension(
-            'roi_align.crop_and_resize_gpu',
-            ['roi_align/src/crop_and_resize_gpu.cpp',
-             'roi_align/src/cuda/crop_and_resize_kernel.cu'],
-            extra_compile_args={'cxx': ['-g', '-fopenmp'],
-                                'nvcc': ['-O2']}
-        )
+modules.append(
+    CUDAExtension(
+        'roi_align.crop_and_resize_gpu',
+        ['roi_align/src/crop_and_resize_gpu.cpp',
+         'roi_align/src/cuda/crop_and_resize_kernel.cu'],
+        extra_compile_args={'cxx': ['-g', '-fopenmp'],
+                            'nvcc': ['-O2']}
     )
+)
 
 setup(
     name='roi_align',
